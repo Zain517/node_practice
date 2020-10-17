1adffb77246d5960362d81592cdf0461b43941ff
“Zain <BSEF17M517@PUCIT.EDU.PK>
2020-10-17 11:20:27 +0500

    Working with Modules done...like module.exports = {} snd to get all these function in desire file use require(PATH)...

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log('Hello node js');
+
+const tutorial = require('./tutorial')
+// console.log(tutorial)
+
+
+// console.log(tutorial(10,3)); // Simple one function call by Module.exports = sum;
+console.log(tutorial.add(10,80));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObjects());
+
+
+
+
+// sum(1,2); 
---
+++ tutorial.js
@@ -0,0 +1,18 @@
+const sum = (num1, num2) => num1 + num2;
+
+const PI = 3.141516
+
+class SomeMathObjects{
+    constructor()
+    {
+        console.log("Constructor Calling..!!!");
+    }
+}
+
+
+
+// module.exports.sum= sum;
+// module.exports.PI = PI;
+// module.exports.SomeMathObjects = SomeMathObjects;
+
+module.exports = {add:sum , PI:PI , SomeMathObjects:SomeMathObjects};
