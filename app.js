13227eebe42f5fe3723a22a4d98b1ef6475c430a
“Zain <BSEF17M517@PUCIT.EDU.PK>
2020-10-17 12:01:58 +0500

    Part#02.... working with the EventEmitter and events like on and emit.....also making function and class etc.....work only in app.js

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 // console.log('Hello node js');
 
-const tutorial = require('./tutorial')
-// console.log(tutorial)
+// const tutorial = require('./tutorial')
+// // console.log(tutorial)
 
 
-// console.log(tutorial(10,3)); // Simple one function call by Module.exports = sum;
-console.log(tutorial.add(10,80));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObjects());
+// // console.log(tutorial(10,3)); // Simple one function call by Module.exports = sum;
+// console.log(tutorial.add(10,80));
+// console.log(tutorial.PI);
+// console.log(new tutorial.SomeMathObjects());
 
 
+//-----------------------------------------------------//
+// Part #02 
+//  Event Modules and class EventEmitter....
+////Event Driven Programming 
 
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+eventEmitter.on('Sum', (num1, num2)=>{
+    console.log('Calling Sum function....!!! ==> Added : ', num1+num2);
+});
+eventEmitter.emit('Sum', 12, 32);
 
-// sum(1,2); 
+class Person extends EventEmitter{
+    constructor(name){
+        super();
+        this._name = name;
+    }
+    get name(){
+        return this._name;
+    }
+}
+
+let P1 = new Person('pedro');
+P1.on('first_name', ()=> { console.log('So the Username of P1 is ' + P1.name ) });
+
+let P2 = new Person('cristina');
+P2.on('first_name', ()=> { console.log('So the Username of P2 is ' + P2.name ) });
+
+P1.emit('first_name');
+P2.emit('first_name');
--- tutorial.js
+++ tutorial.js
@@ -1,18 +1,21 @@
-const sum = (num1, num2) => num1 + num2;
+// const sum = (num1, num2) => num1 + num2;
 
-const PI = 3.141516
+// const PI = 3.141516
 
-class SomeMathObjects{
-    constructor()
-    {
-        console.log("Constructor Calling..!!!");
-    }
-}
+// class SomeMathObjects{
+//     constructor()
+//     {
+//         console.log("Constructor Calling..!!!");
+//     }
+// }
+
+// // module.exports.sum= sum;
+// // module.exports.PI = PI;
+// // module.exports.SomeMathObjects = SomeMathObjects;
+
+// module.exports = {add:sum , PI:PI , SomeMathObjects:SomeMathObjects};
 
 
-
-// module.exports.sum= sum;
-// module.exports.PI = PI;
-// module.exports.SomeMathObjects = SomeMathObjects;
-
-module.exports = {add:sum , PI:PI , SomeMathObjects:SomeMathObjects};
+//-----------------------------------------------------//
+// Part #02 
+//  Event Modules and class EventEmitter....
