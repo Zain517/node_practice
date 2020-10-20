144caa24c78a9ec9be57b014e0d9dd801a8e84ff
Zain517 <zainulabideen4744@gmail.com>
2020-10-20 22:01:40 +0500

    first rename the large data file....then use createReadStream read from file then read data using event data by CHUNK,......same very next write into new file chunks by chunks ...instead of fulll file read or write.....

--- app.js
+++ app.js
@@ -221,3 +221,11 @@
 // Part #06
 // Readable and Writeable Streams....
 
+const fs = require('fs');
+const readStream = fs.createReadStream('./large-data-file.txt'/*,'utf8'*/);
+const writeStream = fs.createWriteStream('./new_file.txt');
+
+readStream.on('data',(chunk)=>{
+    // console.log(chunk); //jitny bufffer hongy utny hi chunk me read kiya
+    writeStream.write(chunk);
+});
