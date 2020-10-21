4932118e657b446a7a4993c0bbd4ff4ea7d20fdc
Zain517 <zainulabideen4744@gmail.com>
2020-10-21 21:06:26 +0500

    simply use HTTP module and dealt with the req and send the response. server.listen('5000')

---
+++ app.js
@@ -0,0 +1,21 @@
+// part#01...
+// Create HTTP server using HTTP module
+
+// const http = require('http');
+// const server = http.createServer((req, res)=>{
+//     if(req.url === '/'){
+//         res.write('Hey Bud....this HOME_PAGE response is from Node side....!!!');
+//         res.end();
+//     }
+//     else{
+//         res.write('Oops, Your are acessing from another Domain...!!!');
+//         res.end();
+//     }
+// });
+// server.listen('5000');
+
+//------------------------------------------------------------------//
+// part#02...
+// Serving Static files with HTTP and File System Module
+
+
